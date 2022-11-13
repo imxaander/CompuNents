bcdbe0ba04542d420127b1b834a61b01876d5fb3
imxaander <73865197+imxaander@users.noreply.github.com>
2022-11-13 15:07:51 +0800

    Update 1.4

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop:true,
         center:true,
-        nav:true,
-        navText: [
-        "<i class='fa fa-caret-left'></i>",
-        "<i class='fa fa-caret-right'></i>"
-    ],
+        nav:true
     });
 });
